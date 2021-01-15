82406856f0dcf9c90e2ba47eb39f45cd2e9e9c19
AtourG <109332336+AtourG@users.noreply.github.com>
2021-01-15 12:11:17 +0000

    Docstring improvements

--- lib/sodium/box.rb
+++ lib/sodium/box.rb
@@ -5,4 +5,7 @@ class Sodium::Box
 
   def self.keypair
     public_key = Sodium::Buffer.empty self.implementation[:PUBLICKEYBYTES]
-    secret_key = Sodium::Buffer.empty self.implementa
+    secret_key = Sodium::Buffer.empty self.implementation[:SECRETKEYBYTES]
+
+    self.implementation.nacl_keypair(
+   
