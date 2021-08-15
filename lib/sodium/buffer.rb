a349d1144c52157552015f7a7cf3088975ef9b31
AtourG <109332336+AtourG@users.noreply.github.com>
2021-08-15 18:32:20 +0000

    Fixing comment.

--- lib/sodium/buffer.rb
+++ lib/sodium/buffer.rb
@@ -204,4 +204,5 @@ class Sodium::Buffer::ZeroingDelegator
 
   def initialize(string, &finalizer)
     # specify class name explicitly, since we're letting the `class`
-    # method delegate to the wrapped 
+    # method delegate to the wrapped object
+    Sodium::Buffer::ZeroingDelegator._mloc
