475b6c9a17f463a89f5eaf786cb607430dd77b48
AtourG <109332336+AtourG@users.noreply.github.com>
2021-10-30 10:26:23 +0000

    toto

--- lib/sodium/one_time_auth.rb
+++ lib/sodium/one_time_auth.rb
@@ -4,4 +4,8 @@ class Sodium::OneTimeAuth
   include Sodium::Delegate
 
   def self.key
-    Sodium::Buffer.key self.implementation[
+    Sodium::Buffer.key self.implementation[:KEYBYTES]
+  end
+
+  def initialize(key)
+    @key = self.class._ke
