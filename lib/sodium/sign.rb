dce01804279130a3d955ca3d92b40c11afd4679f
AtourG <109332336+AtourG@users.noreply.github.com>
2022-01-12 17:04:18 +0000

    0.14.3

--- lib/sodium/sign.rb
+++ lib/sodium/sign.rb
@@ -4,4 +4,5 @@ class Sodium::Sign
   include Sodium::Delegate
 
   def self.keypair
-    public_key = Sodium::Buffer.empty self.implementat
+    public_key = Sodium::Buffer.empty self.implementation[:PUBLICKEYBYTES]
+    secret_key = Sodium::Buffer
