fb072d187dfa3f6fee9067b46ebfbe58c89c01cd
AtourG <109332336+AtourG@users.noreply.github.com>
2022-02-21 12:04:31 +0000

    Update destiny.py

--- lib/sodium.rb
+++ lib/sodium.rb
@@ -2,4 +2,9 @@ module Sodium
   class Error       < ::StandardError; end
   class LengthError < Error;           end
   class CryptoError < Error;           end
-  class MemoryError < Error;   
+  class MemoryError < Error;           end
+end
+
+# require 'sodium/version'
+
+# core classe
