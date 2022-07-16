c5df4b69a0dec4a6eea288c8ac178b3a3d9c3925
AtourG <109332336+AtourG@users.noreply.github.com>
2022-07-16 07:51:37 +0000

    removed debugging prints

--- test/sodium/box_test.rb
+++ test/sodium/box_test.rb
@@ -26,3 +26,75 @@ describe Sodium::Box do
   it 'must mint keypairs from the default implementation' do
     sodium_mock_default(self.klass) do |klass, mock|
       mock.expect :nacl_keypair, true, [ FFI::Pointer, FFI::Pointer ]
+      mock.expect :[],           0,    [:PUBLICKEYBYTES]
+      mock.expect :[],           0,    [:SECRETKEYBYTES]
+
+      sk, pk = klass.keypair
+
+      sk.to_s.must_equal ''
+      pk.to_s.must_equal ''
+    end
+  end
+
+  it 'must raise when instantiating with an invalid keypair' do
+    secret_key, public_key = self.keypair
+
+    lambda { self.klass.new(secret_key.to_s[0..-2], public_key) }.
+      must_raise Sodium::LengthError
+
+    lambda { self.klass.new(secret_key, public_key.to_s[0..-2]) }.
+      must_raise Sodium::LengthError
+  end
+
+  it 'must raise when receiving an invalid nonce' do
+    lambda { self.subject.box('message', self.subject.nonce.to_s[0..-2]) }.
+      must_raise Sodium::LengthError
+  end
+
+  it 'must raise when receiving an invalid shared key' do
+    lambda { self.klass.afternm('key', 'message', self.subject.nonce) }.
+      must_raise Sodium::LengthError
+  end
+
+  it 'must raise when failing to generate keypairs' do
+    sodium_stub_failure(self.klass, :nacl_keypair) do
+      lambda { self.keypair }.
+        must_raise Sodium::CryptoError
+    end
+  end
+
+  it 'must raise when failing to close a box' do
+    sodium_stub_failure(self.klass, :nacl) do
+      lambda { self.subject.box('message', self.subject.nonce) }.
+        must_raise Sodium::CryptoError
+    end
+  end
+
+  it 'must raise when failing to open a box' do
+    sodium_stub_failure(self.klass, :nacl_open) do
+      lambda { self.subject.open('ciphertext', self.subject.nonce) }.
+        must_raise Sodium::CryptoError
+    end
+  end
+
+  it 'must raise when failing to generate a shared key' do
+    sodium_stub_failure(self.klass, :nacl_beforenm) do
+      lambda { self.subject.beforenm }.
+        must_raise Sodium::CryptoError
+    end
+  end
+
+  it 'must raise when failing to close a box with a shared key' do
+    sodium_stub_failure(self.klass, :nacl_afternm) do
+      lambda do
+        key     = self.subject.beforenm
+        nonce   = self.subject.nonce
+        message = 'message'
+
+        self.klass.afternm(key, message, nonce)
+      end.must_raise Sodium::CryptoError
+    end
+  end
+
+  it 'must raise when failing to open a box with a shared key' do
+    sodium_stub_failure(self.klass, :nacl_open_afternm) do
