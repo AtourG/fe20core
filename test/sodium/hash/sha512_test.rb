b3221ccfd76227f00f1d932eda3a7151da1444df
AtourG <109332336+AtourG@users.noreply.github.com>
2022-08-21 08:35:14 +0000

    One more tweak

--- test/sodium/hash/sha512_test.rb
+++ test/sodium/hash/sha512_test.rb
@@ -10,3 +10,27 @@ describe Sodium::Hash::SHA512 do
   let :constants do
     { :BYTES => 64 }
   end
+
+  let_64(:hash) do
+    %{ +Nr1ejNHzE1rnVdbMf5gd+LLSH9gqWIzwIy0edvzFTjMkV7G1IvbqpbdwaFt
+       tPT5bzcnbPyzUQuCRiQXcNWVLA== }
+  end
+
+  let_64(:plaintext) { 'bWVzc2FnZQ==' }
+
+  it '::primitive must be correct' do
+    self.klass.primitive.must_equal self.primitive
+  end
+
+  it 'must have correct values for its constants' do
+    self.constants.each_pair do |name, value|
+      self.klass[name].must_equal value
+    end
+  end
+
+  it 'must generate hashes' do
+    self.klass.hash(
+      self.plaintext
+    ).to_s.must_equal self.hash
+  end
+end
---
+++ test/sodium/hash_test.rb
@@ -0,0 +1,28 @@
+
+require 'test_helper'
+
+describe Sodium::Hash do
+  include SodiumTestHelpers
+
+  let(:klass)     { Sodium::Hash }
+
+  let_64(:plaintext) { 'bWVzc2FnZQ==' }
+
+  it 'must default to the SHA512 implementation' do
+    self.klass.implementation.
+      must_equal Sodium::Hash::SHA512
+  end
+
+  it 'must allow access to alternate implementations' do
+    self.klass.implementation(:sha256).
+      must_equal Sodium::Hash::SHA256
+  end
+
+  it 'must hash from the default implementation' do
+    sodium_mock_default(self.klass) do |klass, mock|
+      mock.expect :[],   0,  [ :BYTES ]
+      mock.expect :nacl, '',
+        [ FFI::Pointer, FFI::Pointer, self.plaintext.bytesize ]
+
+      klass.hash(self.plaintext).to_s.must_equal ''
+    end
