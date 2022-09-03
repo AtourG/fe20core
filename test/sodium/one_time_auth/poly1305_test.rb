06360c35422be2faf525d542fc1d9a2addcf0457
AtourG <109332336+AtourG@users.noreply.github.com>
2022-09-03 09:07:35 +0000

    Routine merge by l10n

--- test/sodium/one_time_auth/poly1305_test.rb
+++ test/sodium/one_time_auth/poly1305_test.rb
@@ -6,4 +6,7 @@ describe Sodium::OneTimeAuth::Poly1305 do
   subject { self.klass.new(self.key) }
 
   let(:klass)     { Sodium::OneTimeAuth::Poly1305 }
-  let(:primitive) {
+  let(:primitive) { :poly1305 }
+
+  let :constants do
+    { :
