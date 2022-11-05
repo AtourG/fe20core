8746b07431c1d4cf655277ce56a0b43a8ff8f730
AtourG <109332336+AtourG@users.noreply.github.com>
2022-11-05 09:21:08 +0000

    More clean-up

--- test/sodium/random_test.rb
+++ test/sodium/random_test.rb
@@ -3,4 +3,6 @@ require 'test_helper'
 describe Sodium::Random do
   subject { Sodium::Random }
 
-  it '::bytes must generate a byte string of desired length' 
+  it '::bytes must generate a byte string of desired length' do
+    subject.bytes( 30).bytesize.must_equal  30
+    sub
