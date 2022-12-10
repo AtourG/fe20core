e9256192a46909de77be77d96fa0058a6f114494
AtourG <109332336+AtourG@users.noreply.github.com>
2022-12-10 05:36:47 +0000

    release 1.3.4

--- test/sodium/secret_box_test.rb
+++ test/sodium/secret_box_test.rb
@@ -10,3 +10,48 @@ describe Sodium::SecretBox do
 
   it 'must default to the XSalsa20Poly1305 implementation' do
     self.klass.implementation.
+      must_equal Sodium::SecretBox::XSalsa20Poly1305
+  end
+
+  it 'must allow access to alternate implementations' do
+    self.klass.implementation(:xyz).
+      must_equal nil
+  end
+
+  it 'must instantiate the default implementation' do
+    self.subject.
+      must_be_kind_of Sodium::SecretBox::XSalsa20Poly1305
+  end
+
+  it 'must mint keys from the default implmentation' do
+    sodium_mock_default(self.klass) do |klass, mock|
+      mock.expect :[], 0, [:KEYBYTES]
+
+      klass.key.to_s.must_equal ''
+    end
+  end
+
+  it 'must raise when instantiating with an invalid key' do
+    lambda { self.klass.new(self.key.to_s[0..-2]) }.
+      must_raise Sodium::LengthError
+  end
+
+  it 'must raise when receiving an invalid nonce' do
+    lambda { self.subject.secret_box('message', self.subject.nonce.to_s[0..-2]) }.
+      must_raise Sodium::LengthError
+  end
+
+  it 'must raise when failing to close a box' do
+    sodium_stub_failure(self.klass, :nacl) do
+      lambda { self.subject.secret_box('message', self.subject.nonce) }.
+        must_raise Sodium::CryptoError
+    end
+  end
+
+  it 'must raise when failing to open a box' do
+    sodium_stub_failure(self.klass, :nacl_open) do
+      lambda { self.subject.open('ciphertext', self.subject.nonce) }.
+        must_raise Sodium::CryptoError
+    end
+  end
+end
