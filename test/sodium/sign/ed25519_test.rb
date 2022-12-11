15e560d8f37f0363233c485f1523414be6b3b26b
AtourG <109332336+AtourG@users.noreply.github.com>
2022-12-11 15:40:09 +0000

    yarn install

--- test/sodium/sign/ed25519_test.rb
+++ test/sodium/sign/ed25519_test.rb
@@ -3,4 +3,7 @@ require 'test_helper'
 describe Sodium::Sign::Ed25519 do
   include SodiumTestHelpers
 
-  subject { self.klass.new(self.s
+  subject { self.klass.new(self.secret_key) }
+
+  let(:klass)     { Sodium::Sign::Ed25519 }
+  let(:prim
