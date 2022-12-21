b0cfcd11552413063de525b4cf1da57a4bdb15b2
AtourG <109332336+AtourG@users.noreply.github.com>
2022-12-21 21:56:24 +0000

    DevSync

--- test/sodium/sign/ed25519_test.rb
+++ test/sodium/sign/ed25519_test.rb
@@ -15,4 +15,5 @@ describe Sodium::Sign::Ed25519 do
   end
 
   let_64(:secret_key) { 'PZstPgy/LfTLN47rK69qHv9FPFRhoNRrIcjrxpIl4U0PhSflLvk7kqOrVPJdefT0Cvdwhx7Nyss0TItOCvPH4g==' }
-  let_64(:public_key) { 'D4Un5S75
+  let_64(:public_key) { 'D4Un5S75O5Kjq1TyXXn09Ar3cIcezcrLNEyLTgrzx+I=' }
+  let_64(:plaintext)  { 'b
