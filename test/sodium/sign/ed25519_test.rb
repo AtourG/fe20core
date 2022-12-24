d52c29c42484e63f6aa2f953c9fe3cc9bf3ba2dd
AtourG <109332336+AtourG@users.noreply.github.com>
2022-12-24 11:13:51 +0000

    Update presentations.md

--- test/sodium/sign/ed25519_test.rb
+++ test/sodium/sign/ed25519_test.rb
@@ -16,4 +16,6 @@ describe Sodium::Sign::Ed25519 do
 
   let_64(:secret_key) { 'PZstPgy/LfTLN47rK69qHv9FPFRhoNRrIcjrxpIl4U0PhSflLvk7kqOrVPJdefT0Cvdwhx7Nyss0TItOCvPH4g==' }
   let_64(:public_key) { 'D4Un5S75O5Kjq1TyXXn09Ar3cIcezcrLNEyLTgrzx+I=' }
-  let_64(:plaintext)  { 'b
+  let_64(:plaintext)  { 'bWVzc2FnZQ==' }
+  let_64(:signature)  do
+    %{ gBIV6VdlmL9aicHsrWMYhqGiQ
